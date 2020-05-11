10c44c55cdce1aa375c9f7f3dda8b7b901828aa6
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2020-05-11 22:54:32 +0900

    set timer to main

--- src/main.ml
+++ src/main.ml
@@ -1,4 +1,8 @@
 open Common
 open Entities
 
-let () = print_int 1
+let () =
+  let start_time = Sys.time () in
+  let _ =
+    Printf.sprintf "turn time: %.4fms" @@ (Sys.time () -. start_time) *. 1000.0 (* milli second *)
+  in ()
