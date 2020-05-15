6e0fbd2127f003228ff70d49b2e4439ca551481f
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2020-05-15 19:29:40 +0900

    Nel を強化

--- src/nel.ml
+++ src/nel.ml
@@ -7,9 +7,15 @@ let nel_of_list: 'a list -> 'a t = fun lst ->
   | [] -> raise EmptyList
   | hd :: rest -> List.fold_left (fun acc next -> Cons (next, acc)) (Last hd) rest
 
+let cons: 'a -> 'a t -> 'a t = fun hd tail -> Cons (hd, tail)
+
 let hd: 'a t -> 'a = function
   | Cons (h, _) -> h
   | Last last -> last
+
+let tl: 'a t -> 'a t = function
+  | Cons (_, rest) -> rest
+  | Last e -> raise EmptyList
 
 let rec last: 'a t -> 'a = function
   | Last last -> last
@@ -28,16 +34,38 @@ let rev: 'a t -> 'a t = function
   | Last last -> Last last
   | Cons (hd, rest) -> fold_left (fun acc e -> Cons (e, acc)) (Last hd) rest
 
+let append: 'a t -> 'a t -> 'a t = fun nel1 nel2 ->
+  fold_left (fun acc e -> cons e acc) nel2 (rev nel1)
+
 let map: ('a -> 'b) -> 'a t -> 'b t = fun f nel ->
   match nel with
   | Last e -> Last (f e)
   | Cons (hd, rest) -> rev @@ fold_left (fun acc e -> Cons (f e, acc)) (Last (f hd)) rest
 
+let iter: ('a -> unit) -> 'a t -> unit = fun f nel -> ignore @@ map f nel
+
+
+(** scanning **)
+let for_all: ('a -> bool) -> 'a t -> bool = fun p nel ->
+  fold_left (fun acc e -> acc && p e) true nel
+
+let exists: ('a -> bool) -> 'a t -> bool = fun p nel ->
+  let rec func: 'a t -> bool = function
+  | Last e -> p e
+  | Cons (hd, rest) -> if p hd then true else func rest
+  in func nel
+
+
+(** searching **)
 (* 述語 p を満たす先頭の要素を返す *)
-let rec find: ('a -> bool) -> 'a t -> 'a option = fun p nlst ->
-  match nlst with
-  | Last last -> if p last then Some last else None
+let rec find: ('a -> bool) -> 'a t -> 'a option = fun p nel ->
+  match nel with
+  | Last e -> if p e then Some e else None
   | Cons (hd, rest) -> if p hd then Some hd else find p rest
+
+let filter: ('a -> bool) -> 'a t -> 'a list = fun p nel ->
+  List.rev @@ fold_left (fun acc e -> if p e then e :: acc else acc) [] nel
+
 
 (* 第二引数を末尾につける *)
 let create: 'a list -> 'a -> 'a t = fun lst last ->
--- test/nel_spec.ml
+++ test/nel_spec.ml
@@ -24,6 +24,14 @@ let hd_test =
         assert_equal_int 1 (hd @@ nel_of_list [1]) )
   ]
 
+let tl_test =
+  "tl" >::: [
+    ("tail を返す" >:: fun _ ->
+        assert_equal (Cons(2, Last 3)) (tl @@ nel_of_list [1; 2; 3]) )
+    ;
+    (* TODO: 空リストが渡ったケースのテスト *)
+  ]
+
 let last_test =
   "last" >::: [
     ("末尾の要素を返す" >:: fun _ ->
@@ -54,6 +62,21 @@ let rev_test =
         assert_equal (Last 1) (rev @@ nel_of_list [1]) )
   ]
 
+let append_test =
+  "append" >::: [
+    ("複数要素x複数要素" >:: fun _ ->
+        let expected = Cons (1, Cons(2, Cons(3, Last 4))) in
+        assert_equal expected (append (nel_of_list [1; 2]) (nel_of_list [3; 4])) )
+    ;
+    ("単一要素x複数要素" >:: fun _ ->
+        let expected = Cons (1, Cons(2, Last 3)) in
+        assert_equal expected (append (nel_of_list [1]) (nel_of_list [2; 3])) )
+    ;
+    ("複数要素x単一要素" >:: fun _ ->
+        let expected = Cons (1, Cons(2, Last 3)) in
+        assert_equal expected (append (nel_of_list [1; 2]) (nel_of_list [3])) )
+  ]
+
 let map_test =
   "map" >::: [
     ("複数要素" >:: fun _ ->
@@ -61,6 +84,33 @@ let map_test =
     ;
     ("単一要素" >:: fun _ ->
         assert_equal (Last 2) (map ((+) 1) @@ nel_of_list [1]) )
+  ]
+
+let iter_test =
+  "iter" >::: []
+
+let for_all_test =
+  "for_all" >::: [
+    ("全ての要素が一致した場合" >:: fun _ ->
+        assert_equal_bool true (for_all (fun n -> n mod 2 = 0) @@ nel_of_list [2; 4; 6]) )
+    ;
+    ("一部の要素が一致しない場合" >:: fun _ ->
+        assert_equal_bool false (for_all (fun n -> n mod 2 = 0) @@ nel_of_list [1; 2; 3]) )
+    ;
+    ("末尾の要素が一致しない場合" >:: fun _ ->
+        assert_equal_bool false (for_all (fun n -> n <= 2) @@ nel_of_list [2; 4; 7]) )
+  ]
+
+let exists_test =
+  "exists" >::: [
+    ("全ての要素が一致する場合" >:: fun _ ->
+        assert_equal_bool true (exists (fun n -> n mod 2 = 0) @@ nel_of_list [2; 4; 6]) )
+    ;
+    ("一部の要素が一致する場合" >:: fun _ ->
+        assert_equal_bool true (exists (fun n -> n mod 2 = 0) @@ nel_of_list [1; 2; 3]) )
+    ;
+    ("全ての要素が一致しない場合" >:: fun _ ->
+        assert_equal_bool false (exists (fun n -> n mod 2 = 0) @@ nel_of_list [1; 3; 5]) )
   ]
 
 
@@ -74,7 +124,20 @@ let find_test =
     ;
     ("条件にマッチする値が無い場合 None を返す" >:: fun _ ->
         assert_equal_intopt None (find (fun x -> x = 4) (nel_of_list [1; 2; 3])) )
+    ]
+
+let filter_test =
+  "filter" >::: [
+    ("一致する要素を残す" >:: fun _ ->
+        assert_equal_intlist [2] (filter (fun n -> n mod 2 = 0) @@ nel_of_list [1; 2; 3]) )
+    ;
+    ("全ての要素が一致する場合" >:: fun _ ->
+        assert_equal_intlist [1; 2; 3] (filter (fun n -> n <= 3) @@ nel_of_list [1; 2; 3]) )
+    ;
+    ("全ての要素が一致しない場合" >:: fun _ ->
+        assert_equal_intlist [] (filter ((=) 0) @@ nel_of_list [1; 2; 3]) )
   ]
+
 
 let create_test =
   "nel_of_list" >::: [
@@ -90,12 +153,18 @@ let _ = run_test_tt_main begin
     "nel.ml" >::: [
       nel_of_list_test;
       hd_test;
+      tl_test;
       last_test;
       fold_left_test;
       length_test;
       rev_test;
+      append_test;
       map_test;
+      for_all_test;
+      exists_test;
+      iter_test;
       find_test;
+      filter_test;
       create_test;
     ]
   end
