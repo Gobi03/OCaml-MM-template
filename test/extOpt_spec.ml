69802d9cfe7f09323021f60f808234d11867d173
Tatsuya Katsushima <t.katsushima@opt.ne.jp>
2020-05-22 23:48:45 +0900

    ExtOpt にテストを追加

--- test/extOpt_spec.ml
+++ test/extOpt_spec.ml
@@ -12,8 +12,34 @@ let get_or_else_test =
         assert_equal_int 42 (get_or_else 57 (Some 42)))
   ]
 
+let get_or_error_test =
+  "get_or_error" >::: [
+    ("Someの場合" >:: fun _ ->
+        assert_equal_int 42 (get_or_error "error" (Some 42)))
+    ;
+    (* None の場合 *)
+  ]
+
+let list_flatten_test =
+  "list_flatten" >::: [
+    ("None は消され、Some の中身が残る" >:: fun _ ->
+        assert_equal_intlist [42; 57] (list_flatten [Some 42; None; Some 57; None]))
+  ]
+
+let show_test =
+  "show" >::: [
+    ("Noneの場合" >:: fun _ ->
+        assert_equal_str "None" (ExtOpt.show Fun.id None))
+    ;
+    ("Someの場合" >:: fun _ ->
+        assert_equal_str "Some 42" (ExtOpt.show string_of_int @@ Some 42))
+  ]
+
 let _ = run_test_tt_main begin
     "option.ml" >::: [
-      get_or_else_test
+      get_or_else_test;
+      get_or_error_test;
+      list_flatten_test;
+      show_test;
     ]
   end
